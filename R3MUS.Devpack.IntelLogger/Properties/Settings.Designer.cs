7b1956b7955fe1e73535b9bfee3e7c7b502b8499
Clyde <Clyde@CLYDE-PC>
2017-04-14 17:49:59 +0100

    Added new channel to watchlist, changed Slack reference to nuget package

--- R3MUS.Devpack.IntelLogger/Properties/Settings.Designer.cs
+++ R3MUS.Devpack.IntelLogger/Properties/Settings.Designer.cs
@@ -66,7 +66,7 @@ namespace R3MUS.Devpack.IntelLogger.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
             "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring>GOTG_Intel</string>\r\n  <string>GOTG_DeployTel</string>\r\n</ArrayOfString>")]
+            "tring>GOTG_Intel</string>\r\n  <string>R3MUS_DeployTel</string>\r\n</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection IntelChannels {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["IntelChannels"]));
